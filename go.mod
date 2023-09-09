270d620d41e64b7170f62fbda2b3d852ad1b1ead
meili <sweetbabyalaska@gmail.com>
2023-09-08 21:45:06 -0800

    init

---
+++ go.mod
@@ -0,0 +1,19 @@
+module go-flatpak
+
+go 1.21.0
+
+require github.com/ktr0731/go-fuzzyfinder v0.7.0
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.3 // indirect
+	github.com/ktr0731/go-ansisgr v0.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"encoding/xml"
+	"fmt"
+	fzf "github.com/ktr0731/go-fuzzyfinder"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type Comp struct {
+	XMLName xml.Name `xml:"components"`
+	App     []struct {
+		ID          string `xml:"id"`
+		Name        string `xml:"name"`
+		Summary     string `xml:"summary"`
+		Description string `xml:"description"`
+	} `xml:"component"`
+}
+
+type track struct {
+	ID      string
+	Name    string
+	Summary string
+}
+
+func fuzzy(component *Comp) {
+	idx, err := fzf.FindMulti(
+		component.App,
+		func(i int) string {
+			return component.App[i].Name
+		},
+		fzf.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			return fmt.Sprintf("Name: %s (%s)\nSummary: %s",
+				component.App[i].Name,
+				component.App[i].ID,
+				component.App[i].Summary)
+		}))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("selected: %v\n", idx)
+	fmt.Printf("flatpak install %s\n", component.App[idx[0]].ID)
+}
+
+func install(pkg string) {
+	fmt.Println(pkg)
+	cmd := exec.Command("flatpak", "install", "-y", "--noninteractive", pkg)
+	cmd.Stdout = os.Stdout
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Sprintf("could not install %s: \n%s\n", pkg, err)
+	}
+}
+
+func askForConfirmation(s string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [y/n]: ", s)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" {
+			return false
+		}
+	}
+}
+
+func main() {
+	xml_file, err := os.Open("/var/lib/flatpak/appstream/flathub/x86_64/active/appstream.xml")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer xml_file.Close()
+
+	var component Comp
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := io.ReadAll(xml_file)
+
+	// fmt.Println(xml.Unmarshal(byteValue, &component))
+	xml.Unmarshal(byteValue, &component)
+
+	// fuzzy(&component)
+
+	// for i := 0; i < len(component.App); i++ {
+	// 	fmt.Println("Name: " + component.App[i].Name)
+	// 	fmt.Println("ID: " + component.App[i].ID)
+	// 	fmt.Println("Summary: " + component.App[i].Summary)
+	// 	fmt.Println("")
+	// }
+
+	idx, err := fzf.FindMulti(
+		component.App,
+		func(i int) string {
+			return component.App[i].Name
+		},
+		fzf.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			return fmt.Sprintf("Name: %s (%s)\nSummary: %s\n%s\n",
+				component.App[i].Name,
+				component.App[i].ID,
+				component.App[i].Description,
+				component.App[i].Summary)
+		}))
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Printf("selected: %v\n", idx)
+	// fmt.Printf("flatpak install %s\n", component.App[idx[0]].ID)
+	fmt.Println(idx)
+	choice := component.App[idx[0]].ID
+	fmt.Println(choice)
+
+	c := askForConfirmation("Would you like to install: " + choice)
+	if c {
+		fmt.Println("OKAY :)")
+		install(choice)
+	} else {
+		fmt.Println("OKAY Maybe next time :)")
+		os.Exit(0)
+	}
+}
