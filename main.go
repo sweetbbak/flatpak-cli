e1183593e0616aeaf76a4869e4cbd13cb83ab50d
meili <sweetbabyalaska@gmail.com>
2023-09-16 19:34:33 -0800

    fixed stdin issue with reading input of flatpak installation command

--- main.go
+++ main.go
@@ -51,9 +51,13 @@ func fuzzy(component *Comp) {
 }
 
 func install(pkg string) {
-	fmt.Println(pkg)
+	fmt.Println("Installing: ", pkg)
+
+	// r, w := io.Pipe()
 	cmd := exec.Command("flatpak", "install", "-y", "--noninteractive", pkg)
-	cmd.Stdout = os.Stdout
+	// cmd.Stdin = r
+
+	cmd.Stdin = os.Stdout
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -75,7 +79,7 @@ func askForConfirmation(s string) bool {
 
 		response = strings.ToLower(strings.TrimSpace(response))
 
-		if response == "y" || response == "yes" {
+		if response == "y" || response == "yes" || response == "\n" {
 			return true
 		} else if response == "n" || response == "no" {
 			return false
@@ -83,7 +87,48 @@ func askForConfirmation(s string) bool {
 	}
 }
 
+func parse_xml() {
+	xml_file, err := os.Open("/var/lib/flatpak/appstream/flathub/x86_64/active/appstream.xml")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer xml_file.Close()
+
+	var component Comp
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := io.ReadAll(xml_file)
+	xml.Unmarshal(byteValue, &component)
+
+	idx, err := fzf.FindMulti(
+		component.App,
+		func(i int) string {
+			return component.App[i].Name
+		},
+		fzf.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			return fmt.Sprintf("Name: %s (%s)\nSummary: %s\n%s\n",
+				component.App[i].Name,
+				component.App[i].ID,
+				component.App[i].Description,
+				component.App[i].Summary)
+		}))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(idx)
+	choice := component.App[idx[0]].ID
+	fmt.Println(choice)
+}
+
 func main() {
+	home := os.Getenv("HOME")
+	fmt.Println(home)
+
 	xml_file, err := os.Open("/var/lib/flatpak/appstream/flathub/x86_64/active/appstream.xml")
 
 	if err != nil {
